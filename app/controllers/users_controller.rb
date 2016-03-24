c9a697395d6804241160347a7b912a40965a71b1
Alicia Prather <Alprather@gmail.com>
2016-03-24 09:13:33 +0400

    sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,26 @@
 class UsersController < ApplicationController
   def new
-     @user = User.new
+    @user = User.new
    end
 
-   def create
-   @user = User.new
-   @user.name = params[:user][:name]
-   @user.email = params[:user][:email]
-   @user.password = params[:user][:password]
-   @user.password_confirmation = params[:user][:password_confirmation]
+  def show
+    @user = User.find(params[:id])
+  end
 
-   if @user.save
-     flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-     redirect_to root_path
-   else
-     flash.now[:alert] = "There was an error creating your account. Please try again."
-     render :new
-   end
- end
+  def create
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      create_session(@user)
+      redirect_to user_path(current_user)
+    else
+      flash.now[:alert] = 'There was an error creating your account. Please try again.'
+      render :new
+    end
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
